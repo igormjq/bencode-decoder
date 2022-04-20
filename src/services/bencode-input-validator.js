bd3d6e4b78351e356929a271f9aa9ca5c8d3f69f
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2022-04-20 19:46:02 -0300

    refactor: update validator return statement

--- src/services/bencode-input-validator.js
+++ src/services/bencode-input-validator.js
@@ -3,7 +3,10 @@ import validator from 'bencode-validator';
 export class InputValidator {
   validate(input) {
     if(typeof input !== 'string') throw new Error('Input must be a string');
-    const isValid = validator(Buffer.from(input));
+    const buffered = Buffer.from(input);
+    const isValid = validator(buffered);
     if(Boolean(isValid)) throw new Error('Invalid input');
+
+    return buffered;
   }
 }
