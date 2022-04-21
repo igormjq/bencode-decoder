bf149a21667e77fcf14b4229d101d8983c9c0720
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2022-04-21 14:17:26 -0300

    config: add module name mapper to resolve import aliases

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
     '<rootDir>/src/**/*.js',
     '!<rootDir>/src/shared/constants.js',
   ],
+  moduleNameMapper: {
+    '@services/(.*)': ['<rootDir>/src/services/$1'],
+    '@shared/(.*)': ['<rootDir>/src/shared/$1'],
+  }
 }
