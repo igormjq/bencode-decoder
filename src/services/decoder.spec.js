35252612b4d0200bd1940517e5d2f584156c3563
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2022-04-21 14:17:45 -0300

    test: implement validation tests for decoder

--- src/services/decoder.spec.js
+++ src/services/decoder.spec.js
@@ -1,5 +1,27 @@
+import { DecoderService } from '@services/decoder'
+import { InputValidator } from '@services/bencode-input-validator'
+
 describe('Services :: Decoder', () => {
-  test('should pass initial test', () => {
-    expect(1).toBe(1)
+  const makeSut = () => new DecoderService();
+
+  beforeEach(() => jest.clearAllMocks())
+
+  describe('#decode', () => {
+    it('should call InputValidator with correct values', () => {
+      const decoder = makeSut();
+      const spyOnValidator = jest.spyOn(InputValidator.prototype, 'validate');
+
+      decoder.decode('3:iso');
+      expect(spyOnValidator).toHaveBeenCalledWith('3:iso');
+    })
+    it('should throw when InputValidator throws', () => {
+      jest.spyOn(InputValidator.prototype, 'validate')
+        .mockImplementationOnce(() => {
+          throw new Error('Any error')
+        })
+      const decoder = makeSut();
+
+      expect(() => decoder.decode('2incorret')).toThrow();
+    })
   })
 })
